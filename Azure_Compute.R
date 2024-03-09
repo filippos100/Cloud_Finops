0bdd1ea94a24805133d07c986357478c32301a75
filippos100 <filipposstamatelos@yahoo.com>
2024-03-09 16:08:27 +0100

    Pushing code

--- Azure_Compute.R
+++ Azure_Compute.R
@@ -1,3 +1,5 @@
 url <- "https://prices.azure.com/api/retail/prices?$filter=serviceFamily%20eq%20%27Compute%27"
 
 Compute <- retrieve_and_unnest_data(url)
+
+Compute <- bind_rows(Compute)
--- Azure_Storage.R
+++ Azure_Storage.R
@@ -1,3 +1,6 @@
 url <- "https://prices.azure.com/api/retail/prices?$filter=serviceFamily%20eq%20%27Storage%27"
 
 Storage <- retrieve_and_unnest_data(url)
+
+Storage <- bind_rows(Storage)
+
